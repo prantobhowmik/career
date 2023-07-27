33f2b2a643ae79a0276db8c6cd853741b87438f1
prantobhowmik <110746217+prantobhowmik@users.noreply.github.com>
2023-07-27 21:10:50 +0000

    fixing base theme

--- app.py
+++ app.py
@@ -2,39 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-jobs = [
-  {
-    'id': 1,
-    'title': 'Frontend Developer',
-    'Location': 'Banglore',
-    'Salary': 'Rs 2,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Backend Developer',
-    'Location': 'Pune',
-    'Salary': 'Rs 1,50,000'
-  },
-  {
-    'id': 3,
-    'title': 'Software Engineer',
-    'Location': 'Vancouver',
-    'Salary': 'CAD 20,000'
-  },
-  {
-    'id': 4,
-    'title': 'HR',
-    'Location': 'Kolkata',
-    'Salary': ''
-  }
-  
-]
+
 
 @app.route("/")
-def bsae():
-  return render_template('base.html', 
-                         job=jobs, 
-                         company_name= 'dope')
+def base():
+  return render_template('base.html'
+                         )
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
