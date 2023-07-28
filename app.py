d21ec1d55925797c9a99a4d4aae6c5009efb886d
prantobhowmik <110746217+prantobhowmik@users.noreply.github.com>
2023-07-28 19:04:37 +0000

    py updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,8 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def base():
-  return render_template('base.html'
+  return render_template('base.html',
+                         pname='Pranto Bhowmik',
+                         footer_name='All rights reserved \u00A9 Pranto Bhowmik 2023'
+                         )
+@app.route("/")
+def contact():
+  return render_template('contact.html', 
+                 pname='Pranto Bhowmik',
+                         footer_name='All rights reserved \u00A9 Pranto Bhowmik 2023'
                          )
 
+  
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
