5fea3a12786c1962a54bf32e366c02c185d2260b
prantobhowmik <110746217+prantobhowmik@users.noreply.github.com>
2023-07-29 19:35:22 +0000

    adding pages

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ def base():
                          pname='Pranto Bhowmik',
                          footer_name='All rights reserved \u00A9 Pranto Bhowmik 2023'
                          )
-@app.route("/")
+@app.route("/app/contact.html")
 def contact():
-  return render_template('contact.html', 
-                 pname='Pranto Bhowmik',
+  return render_template('contact.html',
+                         pname='Pranto Bhowmik',
                          footer_name='All rights reserved \u00A9 Pranto Bhowmik 2023'
                          )
-
   
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,5 @@
+import sqlalchemy
+
+import create_engine
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
